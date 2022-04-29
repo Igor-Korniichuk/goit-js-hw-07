ef94aa231103314de1725d4aaf5fe26a4744c3ed
Igor-Korniichuk <kiv160780@gmail.com>
2022-04-29 21:37:32 +0300

    Создаёт и рендерит разметку ДЗ №7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const divGalleryEl = document.querySelector('.gallery');
+const itemsGalleryMarkup = createItemGalleryMurkup(galleryItems);
+divGalleryEl.insertAdjacentHTML('beforeend', itemsGalleryMarkup);
+console.log(divGalleryEl);
+
+
+console.log(createItemGalleryMurkup(galleryItems));
+
+function createItemGalleryMurkup(galleryItems) {
+        return galleryItems.map(({ preview, original, description }) =>
+        
+        `<div class="gallery__item">
+            <a class="gallery__link"
+                data-lightbox="images"
+                href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"                
+                alt="${description}"
+                />
+            </a>
+        </div>`).join('');    
+};
+
+divGalleryEl.addEventListener('click', onOpenGalleryItem);
+
+function onOpenGalleryItem(event) {
+    if (!event.target.classlist.contains('gallery__image')) {
+        return;
+    }
+    console.log(event.target.dataset.sourse);
+}
+
+
 console.log(galleryItems);
+
+    
+   
+
