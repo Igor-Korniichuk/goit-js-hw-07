1f3c45aa39e7f52e30c91cd92285312a0ca338b7
Igor-Korniichuk <kiv160780@gmail.com>
2022-05-02 18:02:04 +0300

    Выполняет задание 7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const itemsGalleryMarkup = createItemGalleryMurkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', itemsGalleryMarkup);
+console.log(galleryEl)
+
+console.log(createItemGalleryMurkup(galleryItems));
+
+function createItemGalleryMurkup(galleryItems) {
+        return galleryItems.map(({ preview, original, description }) =>        
+        (`<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>`)).join('');      
+};
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    fadeSpeed: 250,
+    overlayOpacity: 0.8,
+    captionsData: "alt",
+});
+
+lightbox.on('show.simplelightbox', function (event) {
+    return; 
+});
+
