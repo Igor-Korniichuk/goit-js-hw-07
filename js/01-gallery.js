223d5a86bb29ac53b617ef658bbced80972d3c61
Igor-Korniichuk <kiv160780@gmail.com>
2022-05-02 16:13:03 +0300

    Открывает и закрывает картинки по клику

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,39 +4,39 @@ import { galleryItems } from './gallery-items.js';
 const divGalleryEl = document.querySelector('.gallery');
 const itemsGalleryMarkup = createItemGalleryMurkup(galleryItems);
 divGalleryEl.insertAdjacentHTML('beforeend', itemsGalleryMarkup);
-console.log(divGalleryEl);
-
+console.log(divGalleryEl)
 
 console.log(createItemGalleryMurkup(galleryItems));
 
 function createItemGalleryMurkup(galleryItems) {
-        return galleryItems.map(({ preview, original, description }) =>
-        
+        return galleryItems.map(({ preview, original, description }) =>        
         `<div class="gallery__item">
-            <a class="gallery__link"
-                data-lightbox="images"
-                href="${original}">
-                <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"                
-                alt="${description}"
-                />
+            <a class="gallery__link" data-lightbox="images"  href="${original}">
+                <img class="gallery__image"
+                src="${preview}" 
+                data-source="${original}"
+                alt="${description}"/>
             </a>
-        </div>`).join('');    
+        </div>`).join('');      
 };
 
 divGalleryEl.addEventListener('click', onOpenGalleryItem);
 
 function onOpenGalleryItem(event) {
-    if (!event.target.classlist.contains('gallery__image')) {
+    
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
         return;
     }
-    console.log(event.target.dataset.sourse);
+        
+    const eventImageEl = event.target.dataset.source;    
+    const instance = basicLightbox.create(`<img src="${eventImageEl}">`);       
+    instance.show();
 }
+    
 
 
-console.log(galleryItems);
+
 
     
    
